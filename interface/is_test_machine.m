1bab232af46162e1566902f4b0f3adbf4f6a61a9
paolotof <varati@hotmail.com>
2015-06-03 15:19:32 +0200

    updated options before interface call

--- interface/is_test_machine.m
+++ interface/is_test_machine.m
@@ -1,9 +1,9 @@
-function b = is_test_machine()
+function [b, s] = is_test_machine()
 
 [c, s] = system('hostname');
 s = strtrim(s);
 switch s
-    case {'hoogglans', 'Nawals-MacBook-Pro.local', 'mw161070.med.rug.nl'}
+    case {'hoogglans', 'Nawals-MacBook-Pro.local', 'mw161070.med.rug.nl', '12-000-4372'}
         b = 0;
     otherwise
         b = 1;
